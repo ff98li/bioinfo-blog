a8ce4b6c193a69bf16d1f426186e73f166e01640
ff98li <li.feifei.official@gmail.com>
2021-05-04 21:02:34 -0400

    moved darkmode script to js

--- docs/js/external-link.js
+++ docs/js/external-link.js
@@ -1,8 +1,8 @@
 (function() {
-  var links = document.getElementsByTagName('a');
-  for (var i = 0; i < links.length; i++) {
-    if (/^(https?:)?\/\//.test(links[i].getAttribute('href'))) {
-      links[i].target = '_blank';
+    var links = document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        if (/^(https?:)?\/\//.test(links[i].getAttribute('href'))) {
+            links[i].target = '_blank';
+        }
     }
-  }
 })();
